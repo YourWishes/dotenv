6368e5bb05f4537fba8c012f59b583d11247d56b
Dominic Masters <dominic@domsplace.com>
2021-11-09 08:48:57 -0800

    Fixed chmod position

--- dotenv.sh
+++ dotenv.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Grant SH execute
-chmod +x ./**/*.sh
-
 # Disable running as root.
 if [[ "$EUID" -eq 0 ]]; then
   echo "Please do not run as root"
   exit 1
 fi
+
+# Grant SH execute
+chmod +x ./**/*.sh
 
 # Arch Linux Setup
 if [ -f "/etc/arch-release" ]; then
