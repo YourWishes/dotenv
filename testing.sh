242a5ec03fd2f78148f71be4e430d9ae8c2665be
Dominic Masters <dominic@domsplace.com>
2022-09-17 13:12:46 -0700

    Testing touchpoints

--- testing.sh
+++ testing.sh
@@ -5,3 +5,8 @@ qdbus org.kde.KWin /VirtualDesktopManager org.kde.KWin.VirtualDesktopManager.row
 qdbus org.kde.KWin /VirtualDesktopManager org.kde.KWin.VirtualDesktopManager.removeDesktop $(qdbus org.kde.KWin /VirtualDesktopManager org.kde.KWin.VirtualDesktopManager.current)
 # TODO: Remove the tray icon
 
+# Enable touch points
+if [[ $(qdbus org.kde.KWin /Effects org.kde.kwin.Effects.isEffectLoaded touchpoints) -eq 'false' ]]; then
+  qdbus org.kde.KWin /Effects org.kde.kwin.Effects.isEffectLoaded touchpoints
+  qdbus org.kde.KWin /Effects org.kde.kwin.Effects.loadEffect touchpoints
+fi
