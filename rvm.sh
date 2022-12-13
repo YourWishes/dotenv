ef68e3f5789d84e7b92420336e5922dfa72e5f92
Dominic Masters <dominic@domsplace.com>
2022-12-13 11:22:18 -0800

    Changed ruby ver

--- rvm.sh
+++ rvm.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 \curl -sSL https://get.rvm.io | bash
 source $HOME/.rvm/scripts/rvm
-rvm install 2.1
+rvm install 2.7.5
+rvm use 2.7.5 --default
 echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && source \"$HOME/.rvm/scripts/rvm\" # Load RVM into a shell session *as a function*" >> $HOME/.bashrc
