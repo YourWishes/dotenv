cd6053ebaf31974196088b94de72f9cc2bd9a29e
Dominic Masters <dominic@domsplace.com>
2022-12-13 11:49:44 -0800

    Fixed ruby jank

--- rvm.sh
+++ rvm.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 \curl -sSL https://get.rvm.io | bash
 source $HOME/.rvm/scripts/rvm
-rvm install 2.7.5
+sudo pacman -S openssl-1.1 -q --noconfirm
+rvm pkg install openssl
+rvm install 2.7.5 --with-openssl-dir=$HOME/.rvm/usr
 rvm use 2.7.5 --default
 echo "[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && source \"$HOME/.rvm/scripts/rvm\" # Load RVM into a shell session *as a function*" >> $HOME/.bashrc
